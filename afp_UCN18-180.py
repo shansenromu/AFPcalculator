b65675a342f7809895d7f958356239ad06f56fac
seanhr <47644750+shansenromu@users.noreply.github.com>
2021-08-31 14:13:17 -0700

    Add files via upload

--- afp_UCN18-180.py
+++ afp_UCN18-180.py
@@ -17,6 +17,10 @@ from scipy.integrate import ode
 import scipy.constants as c
 
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+
+
 from b_grid import bField
 from b_grid import b1Field
 from b_grid import ReadB
@@ -58,7 +62,7 @@ T1=100.0     # seconds
 #neutron stuff
 velocity = -800 #cm/s
 x0 = 130 #cm
-xf = 10
+xf =  10 #cm
 
 # afp parameters
 freq=7.4e3         # b1 frequency (Hz)
@@ -75,9 +79,9 @@ b1scale = 1.16 #B1 gird was calculated with 1 A but actual Amps was 1.16 A
 #db0dt=deltab/deltat # b0 ramp rate (uT/s)
 
 
-t0=0.               # starting time
+t0= 0.               # starting time
 #t1=(xf-x0)/velocity # ending time (seconds)
-t1 =0.1499
+t1 = 0.1499
 dt=.00001           # time step (seconds)
 
 #Starting position
@@ -103,37 +107,40 @@ unitvec =[coeff_vec*vec[0],coeff_vec*vec[1],coeff_vec*vec[2]]
 s0=unitvec      # starting polarization vector
 
 # sparsification factor
-sparse=100000
+sparse=1000000
 i=0
 
 def b(t):
     B0calc = bField(X0D,Y0D,Z0D, B0xField, B0yField, B0zField, x0+velocity*t,yAxis,0,  13,2,2 )  #bField("CourseFieldMap-SCM18-070.txt", x0+velocity*t,-5,0, 13,2,2)
     B1calc = b1Field(X1D,Y1D,Z1D, B1xField, B1yField, B1zField,x0+velocity*t-SFcent,yAxis,0, 44,44,401)  #b1Field("B1grid.tsv", 0,-5.0,x0+velocity*t-30,44,44,401 )
     # need to scale B1 by 1.16
-    bx= B0calc[0] + b1scale*cos(omega*t)*B1calc[2]  #+ b1*cos(omega*t)
-    by= B0calc[1] + b1scale*cos(omega*t)*B1calc[1] 
-    bz= B0calc[2] + b1scale*cos(omega*t)*B1calc[0]              
-    #n=int(t/deltat) # nth up (even) or down (odd)
-    #if(n%2==0):     # up sweep
-    #    bz=b00+db0dt*(t-n*deltat)
-    #else:           # down sweep
-    #    bz=b00+deltab-db0dt*(t-n*deltat)
-    #if (t*dt)/1000.0 % 1 == 0:
-    #   PerDone = t/0.1499 *100.00 
-    #   print ("%.2f"% round(PerDone,2))
-    #   print (bx, by, bz)
-    PerDone = t/0.1499 *100.00 
-    print ("%.2f"% round(PerDone,2))
-    print (bx, by, bz)
+    bx0 = B0calc[0]
+    by0 = B0calc[1]
+    bz0 = B0calc[2]
+    # trying to fix the mismatch of coordinate systems
+    bx1_m = b1scale*B1calc[2]
+    by1_m = b1scale*B1calc[1]
+    bz1_m = b1scale*B1calc[0]
+
+    bx1 = cos(omega*t)*bx1_m
+    by1 = cos(omega*t)*by1_m
+    bz1 = cos(omega*t)*bz1_m
 
 
-    return [bx,by,bz]  # uT
+
+    bx= bx0 + bx1
+    by= by0 + by1 
+    bz= bz0 + bz1
+    
+  
+
+    return [bx,by,bz,bx0,by0,bz0,bx1,by1,bz1,bx1_m,by1_m,bz1_m]  # uT
 
 def dsdt(t,s):
     # The Bloch equations in the non-rotating frame
     # f = dS/dt = gamma S x B + relaxation
     sx,sy,sz=s
-    bx,by,bz=b(t)
+    bx,by,bz,bx0,by0,bz0,bx1,by1,bz1,bx1_m,by1_m,bz1_m=b(t)
     dsxdt=gamma*(sy*bz-sz*by)# -(sx-c*bx)/T2
     dsydt=gamma*(sz*bx-sx*bz)# -(sy-c*by)/T2
     dszdt=gamma*(sx*by-sy*bx)# -(sz-c*bz)/T1
@@ -157,54 +164,75 @@ while r.successful() and r.t < t1:
     #s_demod_x=s_demod_x+(-s_demod_x+s*cos(omega*t))*dt/tau
     #s_demod_y=s_demod_y+(-s_demod_y+s*sin(omega*t))*dt/tau
     i+=1
+  
+    
     if(i%sparse):
-        coeff=1.0/(sqrt(b(t)[0]*b(t)[0]+b(t)[1]*b(t)[1]+b(t)[2]*b(t)[2]))
-        sol.append([t,sx,sy,sz,b(t)[0],b(t)[1],b(t)[2],sx-coeff*b(t)[0],sy-coeff*b(t)[1],sz-coeff*b(t)[2],sqrt(b(t)[0]*b(t)[0]+b(t)[1]*b(t)[1]+b(t)[2]*b(t)[2]),coeff*(sx*b(t)[0]+sy*b(t)[1]+sz*b(t)[2])])
+        
+        bxdt,bydt,bzdt,bx0dt,by0dt,bz0dt,bx1dt,by1dt,bz1dt,bx1_mdt,by1_mdt,bz1_mdt= b(t+10*dt)
+        bx,by,bz,bx0,by0,bz0,bx1,by1,bz1,bx1_m,by1_m,bz1_m= b(t)
+        b_coeff=1.0/(sqrt(bx*bx+by*by+bz*bz))
+        b0_coeff=1.0/(sqrt(bx0*bx0+by0*by0+bz0*bz0))
+        dx = dt/velocity
+        #k =(gamma*( bx1_m*bx1_m + by1_m*by1_m + bz1_m*bz1_m ))/((sqrt(bx0dt*bx0dt+by0dt*by0dt+bz0dt*bz0dt)-sqrt(bx0*bx0+by0*by0+bz0*bz0))/(dt))
+        #print ( gamma*(bx1_m*bx1_m + by1_m*by1_m + bz1_m*bz1_m),(sqrt(bx0dt*bx0dt+by0dt*by0dt+bz0dt*bz0dt)-sqrt(bx0*bx0+by0*by0+bz0*bz0))/(10*dt))#,k )
+
+
+
+        if((sqrt(bx0dt*bx0dt+by0dt*by0dt+bz0dt*bz0dt)-sqrt(bx0*bx0+by0*by0+bz0*bz0))/(10*dt) == 0.0):
+            k = 500.0
+            k0 = 500.0
+        else:
+            k =abs((gamma*( bx1_m*bx1_m + by1_m*by1_m + bz1_m*bz1_m ))/((sqrt(bx0dt*bx0dt+by0dt*by0dt+bz0dt*bz0dt)-sqrt(bx0*bx0+by0*by0+bz0*bz0))/(10*dt)))
+            k0 =abs((gamma*( bx0*bx0 + by0*by0 + bz0*bz0 ))/((sqrt(bx0dt*bx0dt+by0dt*by0dt+bz0dt*bz0dt)-sqrt(bx0*bx0+by0*by0+bz0*bz0))/(10*dt)))
+
+        sol.append([t,sx,sy,sz,bx,by,bz,sx-b_coeff*bx,sy-b_coeff*by,sz-b_coeff*bz,sqrt(bx*bx+by*by+bz*bz),b_coeff*(sx*bx+sy*by+sz*bz),b0_coeff*(sx*bx0+sy*by0+sz*bz0),k,centralb0,k0])
         
         #sol_demod_x.append([s_demod_x[0],s_demod_x[1],s_demod_x[2]])
         #sol_demod_y.append([s_demod_y[0],s_demod_y[1],s_demod_y[2]])
+        PerDone = t/0.1499 *100.00 
+        print ("%.2f"% round(PerDone,2))
         i=0
 
 sol=np.array(sol)
-#sol_demod_x=np.array(sol_demod_x)
-#sol_demod_y=np.array(sol_demod_y)
 
+figure(figsize=(15,15),dpi=120)
 
 plt.plot(sol[:,0],sol[:,1],color="red",label="Sx")
 plt.plot(sol[:,0],sol[:,2],color="green",label="Sy")
 plt.plot(sol[:,0],sol[:,3],color="blue",label="Sz")
-plt.plot(sol[:,0],sol[:,11],color="black",label="Cos theta")
-plt.legend()
-#plt.plot(sol[:,0],sol[:,7],color="blue")
+plt.plot(sol[:,0],sol[:,11],color="black",label="S*B/|B|")
+plt.plot(sol[:,0],sol[:,12],color="goldenrod",label="S*B0/|B0|")
+#plt.legend()
 plt.xlabel('Time (s)')
 plt.ylabel('Spin components and demodulation')
-#plt.plot(sol[:,0],sol_demod_x[:,1])
-#plt.plot(sol[:,0],sol_demod_x[:,2])
-#plt.plot(sol[:,0],sol_demod_y[:,0])
-#plt.plot(sol[:,0],sol_demod_y[:,1])
-#plt.plot(sol[:,0],sol_demod_y[:,2])
-#plt.show()
 
 save1 = "SpinCompnents-AlongY{AlongY:.0f}cmAt{SFOffset:.0f}cmSFOffsent.png"
 plt.savefig(save1.format(AlongY = yAxis, SFOffset = SFoffset))
 plt.close()
 
-#plt.plot(sol[:,0],sol[:,7])
-#plt.plot(sol[:,0],sol[:,8])
-#plt.plot(sol[:,0],sol[:,9])
-#plt.xlabel('Time (s)')
-#plt.ylabel('spin vec - unit vect B component')
-#plt.show()
+figure(figsize=(15,15),dpi=120)
 
 plt.plot(sol[:,0],sol[:,4],color="red",label="Bx")
 plt.plot(sol[:,0],sol[:,5],color="green",label="By")
 plt.plot(sol[:,0],sol[:,6],color="blue",label="Bz")
 plt.plot(sol[:,0],sol[:,10],color="black",label="B tot")
-plt.legend()
+#plt.plot(sol[:,0],sol[:,13],color="goldenrod",label="k")
+plt.plot(sol[:,0],sol[:,14],color="hotpink",label="Central B0")
+#plt.legend()
 plt.xlabel('Time (s)')
 plt.ylabel('B ($\mu$T)')
 #plt.show()
 
 save2 = "MagneticFieldComponents-AlongY{AlongY:.0f}cmAt{SFOffset:.0f}cmSFOffsent.png"
 plt.savefig(save2.format(AlongY = yAxis, SFOffset = SFoffset))
+plt.close()
+
+figure(figsize=(15,15),dpi=120)
+plt.plot(sol[:,0],sol[:,13],color="goldenrod",label="k afp")
+plt.plot(sol[:,0],sol[:,15],color="hotpink",label="k asp")
+plt.xlabel('Time (s)')
+plt.ylabel('K values')
+plt.yscale("log")
+save3 = "AdiabaticConditions-AlongY{AlongY:.0f}cmAt{SFOffset:.0f}cmSFOffsent.png"
+plt.savefig(save3.format(AlongY = yAxis, SFOffset = SFoffset))
 plt.close()
